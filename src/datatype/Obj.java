6cf4e9f2c9e017850a3d033cf28a94be5bf570da
Asus <abhinayajyothi19@gmail.com>
2025-05-28 12:02:22 +0530

    introduction

--- src/datatype/Obj.java
+++ src/datatype/Obj.java
@@ -1,7 +1,13 @@
 package datatype;
 
 public class Obj {
-    public static void main(String[]args){
-
+    public static void main(String[]args) {
+        int age = 21;
+        System.out.println(age);
     }
-}
+        public void add(int num1,int num2){
+            System.out.println(num1+num2);
+
+        }
+    }
+
