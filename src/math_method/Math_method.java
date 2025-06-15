e68e1fab114c70c4c466b1afbe59765d0bbfe5d6
jothilakshmi03 <cjothilakshmi2003@gmail.com>
2025-06-15 10:21:28 +0530

    introduction

---
+++ src/math_method/Math_method.java
@@ -0,0 +1,20 @@
+package math_method;
+
+public class Math_method {
+    public static void main(String[]args){
+        int a=4;
+        int b=10;
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.abs(-65));
+        System.out.println(Math.random());   // from 0 to 1
+        System.out.println(Math.round(6.8626));
+        System.out.println(Math.sqrt(456));
+        System.out.println(Math.pow(3,2));
+        System.out.println(Math.ceil(65.28));  // directly add +1
+        System.out.println(Math.floor(54.98));  // only the whole number without adding anything
+
+
+
+    }
+}
