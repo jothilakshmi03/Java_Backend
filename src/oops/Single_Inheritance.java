ad3ab3a62b99a81c9191a253fc731ad22fc1575d
jothilakshmi03 <cjothilakshmi2003@gmail.com>
2025-06-18 11:34:54 +0530

    introduction

--- src/oops/Single_Inheritance.java
+++ src/oops/Single_Inheritance.java
@@ -1,11 +1,14 @@
 package oops;
 
 class Dad{   // parent class
-    int size=567;
+   static int size=567;
     static String home="chennai";
 
     public void property(){
         System.out.println("the area of the property is "+size+" feet/sq");
+    }
+   private void show(){
+        System.out.println("the area is "+size+" location "+home);
     }
 }
 
@@ -16,18 +19,25 @@ class Daughter extends Dad{  // child class
     public void education(){
         System.out.println("the child was graduated at the age of "+age+" in "+home);
     }
+    public void show2(){
+        property();
+    }
 }
 
 public class Single_Inheritance {
     public static void main(String[]args){
         Dad obj=new Dad();
         obj.property();
+//        obj.show();
 
         Daughter edu=new Daughter();
         edu.education();
         edu.property();
 
-        Daughter.home="banglore";
+        Daughter.home="banglore";  // it will affect the parent class also
+//      Dad.size=57637;
+        edu.size=8999;
+        edu.property();
         edu.education();
 
     }
