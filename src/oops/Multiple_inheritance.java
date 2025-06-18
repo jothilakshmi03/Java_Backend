33060aa7a199be859f5237462c4cefbf2edae544
jothilakshmi03 <cjothilakshmi2003@gmail.com>
2025-06-18 16:49:23 +0530

    introduction

---
+++ src/oops/Multiple_inheritance.java
@@ -0,0 +1,45 @@
+package oops;
+
+//grandfather
+//father
+//daughter
+
+class Animal{
+    int legs=4;
+    String name;
+
+    public static void zoo(){
+        Animal a=new Animal();
+        a.name="tiger";
+        System.out.println("the "+a.name+" has "+a.legs+" legs");
+    }
+}
+
+class Elephant extends Animal{
+    public void eat(){
+        System.out.println("the elephant is eating");
+//        zoo();
+    }
+}
+
+class African_elephant extends  Elephant{
+    public void sleep(){
+        System.out.println("it is sleeping");
+    }
+}
+
+public class Multiple_inheritance {
+    public static void main(String[]args){
+
+        Animal obj=new Animal();
+        obj.zoo();
+
+        Elephant e=new Elephant();
+        e.eat();
+
+        African_elephant ae=new African_elephant();
+        ae.sleep();
+        ae.eat();
+        Animal.zoo();
+    }
+}
