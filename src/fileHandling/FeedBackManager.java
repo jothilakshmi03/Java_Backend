b4d5a964ef71c3350770400d3840960b32c109aa
jothilakshmi03 <cjothilakshmi2003@gmail.com>
2025-07-31 10:55:25 +0530

    introduction

---
+++ src/fileHandling/FeedBackManager.java
@@ -0,0 +1,44 @@
+package fileHandling;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+class FeedBack{
+    private static final String FILE_PATH = "feedback.txt";
+
+    public static void submitFeedback(String username, String feedback) {
+        try {
+            FileWriter writer = new FileWriter(FILE_PATH, true); // true = append mode
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String timeStamp = dtf.format(LocalDateTime.now());
+
+            writer.write("[" + timeStamp + "] Username: " + username + "\n");
+            writer.write("Feedback: " + feedback + "\n");
+            writer.write("--------------------------------------------------\n");
+            writer.close();
+
+            System.out.println("Feedback saved successfully!");
+
+        } catch (IOException e) {
+            System.out.println("An error occurred while saving feedback.");
+            e.printStackTrace();
+        }
+    }
+
+}
+
+public class FeedBackManager {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        FeedBack.submitFeedback("jothi","fdsgyfgusgusz");
+    }
+}
+
+
+
+//[2025-07-30 10:15:23] Username: jothi
+//Feedback: Very useful app. Please add dark mode.
+//        --------------------------------------------------
